80be6ee4b2f164c0cc1fb3c52f906afd8bba3348
sanguk <80368246+UGKONG@users.noreply.github.com>
2021-12-20 15:56:06 +0900

    Update hook.js

--- plugins/hook.js
+++ plugins/hook.js
@@ -666,7 +666,7 @@ export const useDelay = (duration) => {
 }
 
 // FormData 리턴함수
-export const useForm = (data = {}) => {
+export const useForm = (data = {}, files = {}) => {
   if (typeof(data) != 'object') return console.warn('data is not object!!');
 
   let keys = Object.keys(data);
@@ -675,6 +675,11 @@ export const useForm = (data = {}) => {
     let value = Array.isArray(data[key]) ? JSON.stringify(data[key]) : data[key];
     form.append(key, value);
   });
+
+  let key = Object.keys(files)[0];
+  if (!key) return form;
+  if (files[key].length == 0) return form;
+  files[key].forEach((file) => form.append(key, file));
 
   return form;
 }
